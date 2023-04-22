90dcb474977a7d52713136fe32fae0edc3d4eacb
Yashar <Yashar.talat@gmail.com>
2023-04-22 19:39:02 +0330

    array-way

--- React_Totorial/myJs.js
+++ React_Totorial/myJs.js
@@ -1,7 +1,7 @@
 const container = document.createElement('div');
 container.classList.add('container');
 
-function generateCart(name,age) {
+function generateCart(name, age) {
     const cart = document.createElement('div');
     cart.classList.add('cart');
     const Name = document.createElement('span');
@@ -10,11 +10,25 @@ function generateCart(name,age) {
     Age.innerText = `age : ${age}`;
     cart.appendChild(Name);
     cart.appendChild(Age);
-
     return cart;
 }
 
-container.appendChild(generateCart("YasharTalat",20));
-container.appendChild(generateCart("AminTalat",22));
-container.appendChild(generateCart("AmirTalat",8));
+const userData = [{
+    name: "YasharTalat",
+    age: 20
+},
+{
+    name: "Amin",
+    age: 22
+},
+{
+    name: "Amir",
+    age: 8
+}
+];
+
+userData.forEach(data => {
+    container.appendChild(generateCart(data.name, data.age));
+});
+
 document.body.appendChild(container);

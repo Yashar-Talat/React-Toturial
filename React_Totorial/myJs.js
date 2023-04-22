82318e8c0032e25e3e1d55efec62a1c4ea3aa971
Yashar <Yashar.talat@gmail.com>
2023-04-22 19:21:00 +0330

    functionType

--- React_Totorial/myJs.js
+++ React_Totorial/myJs.js
@@ -1,14 +1,20 @@
 const container = document.createElement('div');
 container.classList.add('container');
-const cart = document.createElement('div');
-cart.classList.add('cart');
-const Name = document.createElement('span');
-Name.innerText = "name: YasharTalat";
-const age = document.createElement('span');
-age.innerText = "age: 20";
 
-cart.appendChild(Name);
-cart.appendChild(age);
+function generateCart(name,age) {
+    const cart = document.createElement('div');
+    cart.classList.add('cart');
+    const Name = document.createElement('span');
+    Name.innerText = `name :${name}`;
+    const Age = document.createElement('span');
+    Age.innerText = `age : ${age}`;
+    cart.appendChild(Name);
+    cart.appendChild(Age);
 
-container.appendChild(cart);
+    return cart;
+}
+
+container.appendChild(generateCart("YasharTalat",20));
+container.appendChild(generateCart("AminTalat",22));
+container.appendChild(generateCart("AmirTalat",8));
 document.body.appendChild(container);

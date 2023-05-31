5b8b063468a91368541ce11abe5c509f5086abcb
Yashar <Yashar.talat@gmail.com>
2023-05-31 09:41:10 +0330

    create-clock

--- state/src/components/Clock.jsx
+++ state/src/components/Clock.jsx
@@ -1,12 +1,11 @@
-import React, { Component } from "react";
+import React,{Component} from "react";
 
 export default class Clock extends Component {
   render() {
-    return (
-      <div>
-        <p>Hello Yashar Talat</p>
-        <p>Time is {new Date().toLocaleTimeString()}</p>
-      </div>
-    );
+    return <div>
+      <p>Hello Yashar Talat</p>
+      <p>Time is {new Date().toLocaleTimeString()}</p>
+    </div>
   }
 }
+
